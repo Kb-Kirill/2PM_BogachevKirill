2f8c139ff706ef1f9506e85253b15321c78af1cb
Kirill <KBogachev@stud.kantiana.ru>
2022-02-12 13:51:21 +0200

    Task 2: Numbers

--- code/index.php
+++ code/index.php
@@ -1,4 +1,5 @@
 <?php
+echo "............Task 1: Access by link <br>";
 /* Imagine a lot of code here */
 $very_bad_unclear_name = "15 chicken wings";
 // Write your code here:
@@ -8,3 +9,14 @@ $order .= " with spicy-spicy sause";
 
 // Don't change the line below
 echo "\nYour order is: $very_bad_unclear_name.";
+
+echo "<br><br>.............Task 2: Numbers<br>";
+$anyName = 5537;
+echo "$anyName\n"; 
+echo "<br>";
+$newName = 0.839;
+echo "$newName <br>";
+echo ($anyName - 5525), "<br>";
+$last_month = 1187.23;
+$this_month = 1089.98;
+echo ($last_month - $this_month), "<br>";
