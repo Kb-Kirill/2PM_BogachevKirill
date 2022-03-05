3ba3ed24201f5077269bbfa416b2b28ef66bfc11
Kirill <KBogachev@stud.kantiana.ru>
2022-03-05 23:50:54 +0200

    Task 3

---
+++ code/laba_3/task3/Main.php
@@ -0,0 +1,80 @@
+<?php
+
+//создаем массивы
+$catDir = opendir('categories'); //директория с категориями
+$catList = []; //список категорий
+$adList = []; //массив объявления -
+while ($catName = readdir($catDir))
+{
+    if (is_dir("categories/$catName") && $catName !== '.' && $catName !== '..')
+    {
+        $catList[] = $catName;
+        $adList[$catName] = [];
+    }
+}
+?>
+
+<html>
+<head>
+    <title>Файлы</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+<form action="addAd.php" method="POST">
+    Your ad: <br> <br>
+    Name   <input name="Title" ><br>
+    Email  <input name="Email" ><br>
+    Text   <textarea name="Text" rows="7" cols="40" ></textarea><br>
+    Category:
+    <select name="adCat">
+        <?php
+        foreach ($catList as $v)
+        {
+            echo "<option> $v </option>";
+        }
+        ?>
+    </select><br><br>
+    <input type="submit" value = "ADD">
+</form>
+<br>
+<p> Table Ad </p>
+<table border = 4>
+    <tr>
+        <th> Category </th>
+        <th> Title </th>
+        <th> Email </th>
+        <th> Ad </th>
+    </tr>
+    <?php
+    foreach ($catList as $catName)
+    {
+        $dir = opendir("categories/$catName");
+        while ($Name = readdir($dir))
+            if (is_file('categories/'.$catName."/".$Name) )
+            {
+                $adList[$catName][$Name] = $Name;
+            }
+    }
+
+    foreach ($adList as $catN => $adsN)
+        foreach ($adsN as $v)
+        {
+            $filePath = "categories/" . $catN . "/" . $v ;
+            $file = fopen($filePath, "r");
+            $email = fgets($file);
+            $text = "";
+            while (!feof($file))
+                $text .= fgets($file);
+            fclose($file);
+
+            echo "<tr> <td> $catN </td>",
+            "<td> $v </td>",
+            "<td> $email </td>",
+            "<td> $text </td>";
+        }
+
+    ?>
+</table>
+
+</body>
+</html>
